5bb076590c06656c04c49d63ca56e6b04de54824
MouhaChaieb <chaiiebmohamed@gmail.com>
2021-11-16 02:27:06 +0100

    chaged the block

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'alarm clock',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
